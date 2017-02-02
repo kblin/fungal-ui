1d0b6df92afa0553b0cd7c01176a15d0bf56c795
Kai Blin <kblin@biosustain.dtu.dk>
2017-02-02 15:44:55 +0100

    start: Disable submit button while waiting for the server to respond
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/start/start.js
+++ app/start/start.js
@@ -27,6 +27,8 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
             vm.cf_threshold = 0.6;
 
             vm.submit = function (form) {
+                vm.active_submission = true;
+
                 if (vm.upload_file) {
                     vm.submission.seq = vm.file;
                 } else {
@@ -48,8 +50,10 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                     url: '/api/v1.0/submit',
                     data: vm.submission,
                 }).then(function (resp) {
+                    vm.active_submission = false;
                     $state.go('show.job', { id: resp.data.id });
                 }, function (resp) {
+                    vm.active_submission = false;
                     console.log(resp);
                 }, function (evt) {
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
